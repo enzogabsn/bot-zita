5a6e4a8d1819863349f84fcdea0d0645b38ee43c
Enzo Gabriel <enzogabsn@gmail.com>
2025-02-04 22:19:40 -0300

    sair.py

---
+++ funtions/sair.py
@@ -0,0 +1,14 @@
+import os
+import time as t
+
+def sair():
+    print("Saindo do programa.")
+    t.sleep(2)
+    os.system("cls")
+    print("Saindo do programa..")
+    t.sleep(2)
+    os.system("cls")
+    print("Obrigado por usar nosso programa! Caso tenha alguma dúvida sobre algum comando, leia a wiki do nosso repositório do GitHub.\n\nKornerxes Studios")
+    t.sleep(5)
+    exit()
+    os.system("cls")
