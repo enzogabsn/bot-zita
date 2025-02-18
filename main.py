a67d6bc25a8741fe8784c5857421f764682ff9f8
Enzo Gabriel <enzogabsn@gmail.com>
2025-02-18 14:44:37 -0300

    main.py

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 import pyautogui
 import time as t
 import os
-import keyboard
-import shutil
-import funtions.commands.app, funtions.commands.site, funtions.pressione_q, funtions.sair
-
-
-
+import funtions.commands.app, funtions.commands.site, funtions.press_q, funtions.exit, funtions.commands.click_auto
 
 # Tela de boas-vindas
 
@@ -14,29 +9,31 @@ os.system("cls")
 print("-="*20)
 print("Bem-vindo ao Bot Zita")
 print("-="*20)
-print("Esse programa serve para você automatizar o seu dia a dia somente digitando um comando no terminal, \npara isso, leia no repositório do GitHub para ver todos os comandos que pode ser utilizados.")
-    
-funtions.pressione_q.press_q()  # Chama a função pressione_q
-    
-# Usuário digita o comando desejado
+print("Este programa permite automatizar suas tarefas diárias simplesmente digitando um \ncomando no terminal. Por favor, leia o repositório do GitHub para ver todos os comandos que podem ser usados e deixe\numa estrela para nos apoiar. \n\nAss: Kornerxes Studios")
+t.sleep(7)
+funtions.press_q.press_q()  # Chama a função press_q
 
-input_usuario = str(input("Digite o comando desejado: "))
+# Usuário insere o comando desejado
+
+user_input = str(input("Digite o comando desejado: "))
 os.system("cls")
 
-# Abre um aplicativo
-if input_usuario == "/aplicativo":
+# Abrir um aplicativo
+if user_input == "/aplicativo":
     funtions.commands.app.app()  # Chama a função app no arquivo app.py
 
-# Abre um site
-elif input_usuario == "/site":
-    funtions.commands.site.site() # Chama a função site no arquivo site.py
-    
-    # Sai do programa
-elif input_usuario == "/exit":
-    funtions.sair.sair()  # Chama a função sair no arquivo sair.py
+# Abrir um site
+elif user_input == "/pesquisar":
+    funtions.commands.site.site()  # Chama a função site no arquivo site.py
 
+# Sair do programa
+elif user_input == "/sair":
+    funtions.exit.exit()  # Chama a função exit no arquivo exit.py
+
+elif user_input == "/click":
+    funtions.commands.click_auto.click_auto()  # Chama a função click_auto no arquivo click_auto.py
 else:
     print("Erro: Comando não encontrado.")
     t.sleep(2)
     os.system("cls")
-    funtions.sair.sair()  # Chama a função sair no arquivo sair.py
+    funtions.exit.exit()  # Chama a função exit no arquivo exit.py
