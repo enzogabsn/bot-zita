94dbf2c39f0a62b9fda60f80920abd366dde65b5
Enzo Gabriel <enzogabsn@gmail.com>
2025-02-22 10:20:30 -0300

    pegar_posicao.py

--- funtions/pegar_posicao.py
+++ funtions/pegar_posicao.py
@@ -5,27 +5,30 @@ from os import system
 def pegar_posicao():
     while True:
         system("cls")
-        confirm = input("Você deseja pegar a posição do seu mouse?\n\nS/N ")
-        if confirm == "S":
-            pass
-        elif confirm == "N":
+        pegar_posicao = input("\033[35mDeseja pegar a posição do seu mouse? \n\nS/N \033[0m")
+        if pegar_posicao == "S":
+            posicao = pyautogui.position()
+            system("cls")
+            print("3")
+            sleep(1)
+            system("cls")
+            print("2")
+            sleep(1)
+            system("cls")
+            print("1")
+            sleep(1)
+            system("cls")
+            print(f"Posição: \033[32mx={posicao.x}, y={posicao.y}\033[0m")
+            sleep(2)
+            print("\nDigite Enter para terminar o programa")
+            input_temp = input("\n")
+            system("cls")
+            break
+        elif pegar_posicao == "N":
+            system("cls")
             break
         else:
-            print("\033[31mERRO: Você deve escolher uma das opções ditas: S para Sim e N para não\033[0m")
+            system("cls")
+            print("\033[91mERRO: Você deve digitar S para Sim e N para Não\033[0m")
+            sleep(2)
             continue
-        posicao = pyautogui.position()
-        system("cls")
-        sleep(1)
-        print("3.")
-        sleep(1)
-        print("2")
-        sleep(1)
-        print("1")
-        sleep(1)
-        system("cls")
-        print(f"Posição: {posicao}")
-        sleep(2)
-        print("Digite Enter para terminar o programa")
-        input_temp = input()
-        system("cls")
-        break
