99b65127c7e6ccd2554f5fd4bf7231c5513b99fa
Enzo Gabriel <enzogabsn@gmail.com>
2025-02-22 10:21:49 -0300

    sair.py

--- funtions/sair.py
+++ funtions/sair.py
@@ -2,13 +2,11 @@ import os
 import time as t
 
 def sair():
-    print("Saindo do programa.")
-    t.sleep(2)
     os.system("cls")
-    print("Saindo do programa..")
-    t.sleep(2)
+    print("\033[32mObrigado por usar nosso programa! Caso tenha alguma dúvida sobre algum comando, leia a nossa documentação na pasta \033[0m'docs'\033[32m do repositório.\nEstamos gratos por usar o nosso programa de automação, obrigado por usá-lo!\033[0m")
+    print("\n\033[32mAss: \033[0mKornerxes Studios")
+    t.sleep(6.5)
+    print("\nPressione Enter para encerrar o programa")
+    input_enter = input("\n")
     os.system("cls")
-    print("Obrigado por usar nosso programa! Caso tenha alguma dúvida sobre algum comando, leia a wiki do nosso repositório do GitHub.\n\nKornerxes Studios")
-    t.sleep(5)
     exit()
-    os.system("cls")
