6ecabb91db97d8bb0822e17c49caced964d35314
Enzo Gabriel <enzogabsn@gmail.com>
2025-02-22 10:18:30 -0300

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import pyautogui
 import time as t
 import os
-import funtions.pegar_posicao
-import funtions.app, funtions.site, funtions.click_auto
+import funtions.app, funtions.site, funtions.click_auto, funtions.pegar_posicao, funtions.exit
 
 # Tela de boas-vindas
 while True:
@@ -12,29 +11,32 @@ while True:
     print("\033[31m                              -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\033[0m")
     print("")
     print("Este programa permite automatizar suas tarefas diárias simplesmente digitando um \ncomando no terminal. Por favor, leia o repositório do GitHub para ver todos os comandos que podem ser usados e deixe\numa estrela para nos apoiar.")
-    print("\033[34m\nAss: Kornerxes Studios\033[0m")
+    print("\nAss: \033[34mKornerxes Studios\033[0m")
     print("\nPressione Enter para continuar")
     input_temp = input()
     os.system("cls")
     # Usuário insere o comando desejado
-    while True:
-        user_input = input("\033[33mDigite o comando desejado: \033[0m")
-        os.system("cls")
 
-        # Abrir um aplicativo
+    while True:
+        os.system("cls")
+        user_input = input("\033[33mDigite o comando desejado: \033[0m")
         if user_input == "/aplicativo":
             funtions.app.app()  # Chama a função app no arquivo app.py
-        # Abrir um site
+
         elif user_input == "/site":
             funtions.site.abrir_site()  # Chama a função site no arquivo site.py
+
         elif user_input == "/click":
             funtions.click_auto.click_automatico()  # Chama a função click_auto no arquivo click_auto.py
-        elif user_input == "/pegar_posicao":
-            funtions.pegar_posicao.pegar_posicao()
+
+        elif user_input == "/posicao":
+            funtions.pegar_posicao.pegar_posicao() # Chama a função pegar_posicao no arquivo pegar_posicao.py
+
         elif user_input == "/sair":
-            funtions.exit.sair()
+            funtions.exit.sair() # Chama a função sair no arquivo exit.py
         else:
+            os.system("cls")
             print("\033[91mERRO: Você deve digitar um dos comandos válidos\033[0m") # Sistema para repetir o loop de verificação de comando do programa
-            t.sleep(2.7)
+            t.sleep(2)
             os.system("cls")
             continue
